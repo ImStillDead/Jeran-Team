1421d3ca5c70b56877201800ce9e6289e6c9947e
jacobkia <jacobsmykia@gmail.com>
2026-02-07 15:07:47 -0600

    Update gamemanager.cs

--- TeamProject/JeranTeamProject/Assets/scrips/gamemanager.cs
+++ TeamProject/JeranTeamProject/Assets/scrips/gamemanager.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
 
+
 public class gamemanager : MonoBehaviour
 {
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPauese;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
