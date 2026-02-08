e2b40dced004654e72226e7a5f16d8dec600fdd1
jacobkia <jacobsmykia@gmail.com>
2026-02-08 12:50:12 -0600

    update

--- TeamProject/JeranTeamProject/Assets/scrips/gamemanager (Mykia Jacobs).cs
+++ TeamProject/JeranTeamProject/Assets/scrips/gamemanager (Mykia Jacobs).cs
@@ -5,6 +5,10 @@ public class gamemanager : MonoBehaviour
 {
     [SerializeField] GameObject menuActive;
     [SerializeField] GameObject menuPauese;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public bool isPaused;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
