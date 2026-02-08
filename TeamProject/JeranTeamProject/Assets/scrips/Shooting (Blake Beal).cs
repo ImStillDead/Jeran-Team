23f5e0c9a2849da5f58540d749711a7d8ccc5cd0
BlakeBeal10 <blakebeal10@gmail.com>
2026-02-08 13:47:51 -0500

    Commit to test something for Cris

---
+++ TeamProject/JeranTeamProject/Assets/scrips/Shooting (Blake Beal).cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class Shooting : MonoBehaviour
+{
+    // [SerializeFields] for variables that we want to edit in Unity
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDistance;
+    [SerializeField] int shootRate;
+    [SerializeField] int magSizeMax;
+    [SerializeField] GameObject bullet;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
