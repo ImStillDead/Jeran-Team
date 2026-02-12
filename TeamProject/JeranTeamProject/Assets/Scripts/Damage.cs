723677a69ac8983a6c419bb937350af92c58a863
BlakeBeal10 <blakebeal10@gmail.com>
2026-02-12 01:11:25 -0500

    Shooting Commented
    Added Comments to the Shooting Class
    and cleaned it up a bit

--- TeamProject/JeranTeamProject/Assets/Scripts/Damage.cs
+++ TeamProject/JeranTeamProject/Assets/Scripts/Damage.cs
@@ -3,22 +3,27 @@ using System.Collections;
 
 public class Damage : MonoBehaviour
 {
-    enum damageType { bullet, stationary, DOT }
+    enum damageType { bullet, stationary, DOT }     // enum for damage types
+    
+    // [SerializeFields] for variables we to edit in Unity
     [SerializeField] damageType type;
     [SerializeField] Rigidbody rb;
     [SerializeField] LayerMask ignoreLayer;
-
     [SerializeField] int damageAmount;
     [SerializeField] float damageRate;
     [SerializeField] int speed;
     [SerializeField] float destroyTime;
     [SerializeField] ParticleSystem hitEffect;
 
+    // other variables
     bool isDamaging;
 
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /*  Checks if the damageType is bullet. If so, it sets the velocity of the ridgedBody (bullet) 
+            equal to the transform.forward (the direction) times the speed variable. */ 
         if (type == damageType.bullet)
         {
             rb.linearVelocity = transform.forward * speed;
@@ -28,6 +33,7 @@ public class Damage : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        /*  Case for triggers to ignore other triggers*/
         if (other.isTrigger)
         {
             return;
--- TeamProject/JeranTeamProject/Assets/Scripts/Shooting.cs
+++ TeamProject/JeranTeamProject/Assets/Scripts/Shooting.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting;
 
 
 public class Shooting : MonoBehaviour
@@ -28,7 +29,7 @@ public class Shooting : MonoBehaviour
     {
         instance = this;
         maxAmmo = magSizeMax;   // Sets maxAmmo to the maximum mag size
-        currentAmmo = maxAmmo;
+        currentAmmo = maxAmmo;  // Sets currentAmmo equal to the maxAmmo
     }
 
     // Update is called once per frame
@@ -36,11 +37,15 @@ public class Shooting : MonoBehaviour
     {
         shootTimer += Time.deltaTime;
        
+        /*  Gets the input of the fire button and checks if the shoot timer is greater than
+            equal to the shoot rate. If it is it calls the Shoot() method(function) */
         if (Input.GetButton("Fire1") && shootTimer >= shootRate)
         {
             Shoot();
         }
        
+        /*  Checks to see if currentAmmo is less than or equal to 0 and if the player is not reloading.
+            If so, it calls the Reload() method(function) */    
         if (currentAmmo <= 0 && !reloading)
         {       
             StartCoroutine(Reload());       
@@ -48,8 +53,11 @@ public class Shooting : MonoBehaviour
 
     }
 
+    // Called in Update if the Fire1 button (Left Click) is pressed
     public void Shoot()
     {
+        /*  Checks to see if the player is not reloading. If they are not, it fires a projectile
+            and decreases current ammo by 1 */
         if(!reloading)
         {
             shootTimer = 0;
@@ -59,14 +67,15 @@ public class Shooting : MonoBehaviour
 
     }
 
+    // Called in Update if the currentAmmo is less than or equal to 0 and the player is not reloading
     IEnumerator Reload()
     {
 
-        reloading = true;
+        reloading = true;                               // Sets reloading to true to stop the player from firing
 
-        yield return new WaitForSeconds(reloadTime);
-        currentAmmo = maxAmmo;
+        yield return new WaitForSeconds(reloadTime);    // Waits for a set amount of time determined by the reloadTime
+        currentAmmo = maxAmmo;                          // Sets currentAmmo equal to the max ammo
 
-        reloading = false;
+        reloading = false;                              // Sets reloading back to false so the player can shoot again
     }
 }
