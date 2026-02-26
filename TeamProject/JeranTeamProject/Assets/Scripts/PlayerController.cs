7381bd3848fcf0115214a5505ee54a2790c277ae
ImStillDead <Jeranman16@gmail.com>
2026-02-25 21:38:47 -0600

    gun work

--- TeamProject/JeranTeamProject/Assets/Scripts/PlayerController.cs
+++ TeamProject/JeranTeamProject/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class PlayerController : MonoBehaviour, IDamage, IPickup
+public class PlayerController : MonoBehaviour, IDamage, IPickup, IGunPickup
 {
     public PlayerController instancePlayer;
 
@@ -46,7 +46,7 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
         isFirstPerson = true;
         updatePlayerUI();
         invPos = 0;
-        changeGun(gunList[0]);
+        //changeGun(gunList[0]);
         torch.SetActive(true);
         torchActive = true;
     }
@@ -163,7 +163,8 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
     void changeItem(int pos)
     {
         activePick = itemPickup[pos];
-        GameManager.instance.updateItem(activePick.itemIndex);
+        itemIndex = itemPickup[pos].itemIndex;
+        GameManager.instance.updateItem(itemIndex);
     }
     void useItem()
     {
@@ -178,6 +179,7 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
         if(activePick.ammo > 0)
         {
             gunList[gunPos].maxAmmo += activePick.ammo;
+            Shooting.instance.callAmmo();
         }
         if(activePick.dmgBoost > 0)
         {
@@ -204,18 +206,7 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
             }
         }
     }
-    public void changeGun(GunStats gunStats)
-    {
-        if (dmgBoosting)
-        {
-            gunList[gunPos].bullet.damageAmount = tempOrginDmg;
-            dmgBoosting = false;
-        }
-        
-        Shooting.instance.changeGun(gunStats);
-        Shooting.instance.changeBullet();
-     
-    }
+   
     void SwitchWeapon()
     {
         if (Input.GetAxis("Mouse ScrollWheel") > 0 && invPos < itemPickup.Count - 1)
@@ -233,17 +224,17 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
         if (Input.GetButtonDown("Weapon1"))
         {
             gunPos = 0;
-            changeGun(gunList[gunPos]);
+            Shooting.instance.changeGun(gunList[gunPos]);
         }
         else if (Input.GetButtonDown("Weapon2"))
         {
             gunPos = 1;
-            changeGun(gunList[gunPos]);
+            Shooting.instance.changeGun(gunList[gunPos]);
         }
         else if (Input.GetButtonDown("Weapon3"))
         {
             gunPos = 2;
-            changeGun(gunList[gunPos]);
+            Shooting.instance.changeGun(gunList[gunPos]);
         }
     }
     void WeaponRotate()
@@ -322,5 +313,15 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup
         speed *= (int)activePick.speedBoost;
         yield return new WaitForSeconds((float)boostTime);
         speed = tempOrginSpeed;
+    }
+
+    public void GetGunStats(GunStats gun)
+    {
+        gunList.Add(gun);
+        gunPos = gunList.Count - 1;
+        if(gunList.Count == 1)
+        {
+            Shooting.instance.changeGun(gunList[gunPos]);
+        }
     }
 }
