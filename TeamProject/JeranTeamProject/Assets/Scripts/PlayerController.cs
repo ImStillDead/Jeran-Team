7036c526840b4401a0064511c9377d62f17c293e
cris aguilar <caguilar1@student.fullsail.edu>
2026-03-12 19:19:43 -0500

    small fix to prevent no move bug

--- TeamProject/JeranTeamProject/Assets/Scripts/PlayerController.cs
+++ TeamProject/JeranTeamProject/Assets/Scripts/PlayerController.cs
@@ -288,7 +288,10 @@ public class PlayerController : MonoBehaviour, IDamage, IPickup, IGunPickup
     }
     public void updatePlayerUI()
     {
-        GameManager.instance.PlayerHP_bar.fillAmount = (float)HP / HPMax;
+        if (GameManager.instance != null && GameManager.instance.PlayerHP_bar != null)
+        {
+            GameManager.instance.PlayerHP_bar.fillAmount = (float)HP / HPMax;
+        }
     }
 
     // Item Interactions
