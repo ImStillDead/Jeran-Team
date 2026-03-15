92c490a8262f15a6857c55117ba4a5b8294a0dba
jacobkia <jacobsmykia@gmail.com>
2026-03-15 17:52:25 -0500

    multiple text bubbles

--- TeamProject/JeranTeamProject/Assets/Scripts/InteractableTextBubble/NPCInteract.cs
+++ TeamProject/JeranTeamProject/Assets/Scripts/InteractableTextBubble/NPCInteract.cs
@@ -7,32 +7,33 @@ public class NPCInteract : MonoBehaviour, iInteract
     [SerializeField] private TMP_Text bubbleText;
 
     [TextArea(2, 5)]
-    [SerializeField] private string message = "Hello there.";
+    [SerializeField] private string[] message;
 
-    //[SerializeField] private bool useWorldBubble = true;
     [SerializeField] private bool useGameManagerDialog = true;
 
-
     private bool bubbleShowing = false;
+    private int currentPage = 0;
 
     GameManager manager;
 
-
     void Start()
     {
-
-        if (bubbleText != null)
-        { 
-            bubbleText.text = message;
-        }
-
         manager = GameManager.instance;
 
+        if (bubbleObject != null)
+        {
+            bubbleObject.SetActive(false);
+        }
+
+        if (bubbleText != null && message != null && message.Length > 0)
+        { 
+            bubbleText.text = message[0];
+        }
     }
 
     private void Update()
     {
-        if (bubbleShowing && bubbleObject != null)
+        if (bubbleShowing && bubbleObject != null && manager != null)
         {
             manager.guiAlwaysFacePlayer(bubbleObject);
         }
@@ -40,29 +41,49 @@ public class NPCInteract : MonoBehaviour, iInteract
 
     public void Interacted() 
     {
-
-        bubbleShowing = !bubbleShowing;
-
-        if (bubbleObject != null)
-            bubbleObject.SetActive(bubbleShowing);
-
+        // If there is no message, do nothing
+        if (message == null || message.Length == 0)
+            return;
+        // If the bubble is not currently showing, show it and display the first page of the message
+        if (!bubbleShowing)
+        {
+            bubbleShowing = true;
+            if (bubbleObject != null)
+            {
+                bubbleObject.SetActive(true);
+            }
+            ShowPage();
+            return;
+        }
+        // If the bubble is currently showing, advance to the next page of the message
+        currentPage++;
+        if(currentPage < message.Length)
+        {
+            ShowPage();
+        }
+        else
+        {
+            StopInteraction();
+        }
+    }
+    private void ShowPage()
+    {
         if (bubbleText != null)
-            bubbleText.text = message;
-
-        if (useGameManagerDialog && GameManager.instance != null)
-            manager.addDialog(message);
-
+            bubbleText.text = message[currentPage];
+        if (useGameManagerDialog && manager != null)
+        {
+            
+            manager.addDialog(message[currentPage]);
+        }
 
     }
 
     public void StopInteraction()
     {
+        bubbleShowing = false;
+        currentPage = 0;
+
         if (bubbleObject != null)
-        {
-
-            bubbleShowing = false;
             bubbleObject.SetActive(false);
-        }
     }
-
 }
