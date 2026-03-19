8b522acd06bcb96c855a95922778d56b1de7347b
jacobkia <jacobsmykia@gmail.com>
2026-03-18 20:53:24 -0500

    NPC - Volume update

--- TeamProject/JeranTeamProject/Assets/Scripts/Music/AudioSettingsController.cs
+++ TeamProject/JeranTeamProject/Assets/Scripts/Music/AudioSettingsController.cs
@@ -8,28 +8,32 @@ public class AudioSettingsController : MonoBehaviour
 
     private void Start()
     {
-        SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100));
+        float savedVolume = PlayerPrefs.GetFloat("SavedMasterVolume", 1f);
+        SetVolume(savedVolume);
     }
 
     public void SetVolume(float value)
     {
-        if (value < 1)
-            value = 0.001f;
+        if (value < 0.0001f)
+            value = 0.0001f;
 
         RefreshSlider(value);
 
         PlayerPrefs.SetFloat("SavedMasterVolume", value);
+        PlayerPrefs.Save();
 
-        masterMixer.SetFloat("MasterVolume", Mathf.Log10(value / 100) * 20f);
+        masterMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20f);
     }
 
     public void SetVolumeFromSlider()
     {
-        SetVolume(soundsSlider.value);
+        if (soundsSlider != null)
+            SetVolume(soundsSlider.value);
     }
 
     public void RefreshSlider(float value)
     {
-        soundsSlider.value = value;
+        if (soundsSlider != null)
+            soundsSlider.value = value;
     }
 }
