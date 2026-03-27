39010df5017d4a072422e94b99999a4a1dbd1c45
Louie4700 <rookie4700@gmail.com>
2026-03-27 01:42:59 -0400

    Loading level works again

--- TeamProject/JeranTeamProject/Assets/Scripts/buttonFunctions.cs
+++ TeamProject/JeranTeamProject/Assets/Scripts/buttonFunctions.cs
@@ -1,124 +1,205 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ButtonFunctions : MonoBehaviour
 {
     GameManager manager;
 
+    [Header("Loading Screen UI")]
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider progressSlider;
+    [SerializeField] private Text progressText;
+
+    [Header("Loading Settings")]
+    [SerializeField] private float minimumLoadTime = 2f;  
+    [SerializeField] private float sliderSmoothingSpeed = 5f;  
+
+    private static ButtonFunctions instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (loadingScreen != null)
+            loadingScreen.SetActive(false);
+    }
 
     public void Start()
     {
         manager = GameManager.instance ?? FindAnyObjectByType<GameManager>();
     }
+
     public void Resume()
     {
-        manager.menus.stateUnpause();
+        if (manager != null && manager.menus != null)
+            manager.menus.stateUnpause();
     }
-    public void Save()
-    {
 
-    }
-    public void LevelSelect()
-    {
-    }
-    public void Settings()
-    {
-        
-    }
+    public void Save() { }
+    public void LevelSelect() { }
+    public void Settings() { }
+
     public void Respawn()
     {
-        manager.playerScript.SpawnPlayer();
-        manager.menus.stateUnpause();
-    }
-    public void MainMenu()
-    {
-        DataManager.instance.SaveData(DataManager.instance.hubData);
-        manager.UpdateRun();
-        SceneManager.LoadScene(0);
-    }
-    public void ContinueRun()
-    {
-        if (DataManager.instance.currentRun != null)
-        {
-            GameData load = DataManager.instance.currentRun.LoadRun();
-            SceneManager.LoadScene(load.sceneIndex);
-            manager.menus.stateUnpause();
-            manager.LoadRun();
-
-        }
-    }
-    public void ChooseRun(string fileName)
-    {
-        DataManager.instance.fileName = fileName;
-        DataManager.instance.currentLoad.LoadGame();
-    }
-    public void Restart()
-    {
-        if (GameManager.instance == null)
-        {
-            //Debug.LogError("GameManager.instance is null! Cannot restart.");
-            return;
-        }
-        if (manager.playerScript != null)
+        if (manager != null && manager.playerScript != null)
         {
             manager.playerScript.SpawnPlayer();
+            manager.menus?.stateUnpause();
         }
-        
     }
+
+    public void MainMenu()
+    {
+        DataManager.instance?.SaveData(DataManager.instance.hubData);
+        manager?.UpdateRun();
+        LoadSceneWithProgress(0);
+    }
+
+    public void ContinueRun()
+    {
+        if (DataManager.instance != null && DataManager.instance.currentRun != null)
+        {
+            GameData load = DataManager.instance.currentRun.LoadRun();
+            LoadSceneWithProgress(load.sceneIndex);
+            manager?.menus?.stateUnpause();
+            manager?.LoadRun();
+        }
+    }
+
+    public void ChooseRun(string fileName)
+    {
+        if (DataManager.instance != null)
+        {
+            DataManager.instance.fileName = fileName;
+            DataManager.instance.currentLoad?.LoadGame();
+        }
+    }
+
+    public void Restart()
+    {
+        if (manager?.playerScript != null)
+            manager.playerScript.SpawnPlayer();
+    }
+
     public void Quit()
     {
         if (manager != null)
         {
-            try
-            {
-                manager.UpdateRun();
-            }
-            catch (Exception e)
-            {
-                Debug.LogWarning("Failed to save on quit: " + e.Message);
-            }
+            try { manager.UpdateRun(); }
+            catch (Exception e) { Debug.LogWarning("Failed to save: " + e.Message); }
         }
 
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #else
+#else
         Application.Quit();
-        #endif
+#endif
     }
+
     public void NewGame()
     {
-        DataManager.instance.NewGame();
-        SceneManager.LoadScene(1);
+        DataManager.instance?.NewGame();
+        LoadSceneWithProgress(2);
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene(1);
+        LoadSceneWithProgress(2);
     }
+
     public void NextLevel()
     {
         int index = SceneManager.GetActiveScene().buildIndex;
         index += 1;
+
         if (index >= SceneManager.sceneCountInBuildSettings)
         {
-            DataManager.instance.SaveData(DataManager.instance.hubData);
-            manager.UpdateRun();
-            SceneManager.LoadScene(0);
+            DataManager.instance?.SaveData(DataManager.instance.hubData);
+            manager?.UpdateRun();
+            LoadSceneWithProgress(0);
         }
         else
         {
-            manager.UpdateRun();
-            SceneManager.LoadScene(index);
-            manager.sceneIndex = index;
-            manager.resetObjective();
-            manager.playerScript.SpawnPlayer();
-            manager.playerScript.LoadRun();
-            manager.menus.stateUnpause();
+            LoadSceneWithProgress(index);
+            if (manager != null)
+            {
+                manager.sceneIndex = index;
+                manager.resetObjective();
+                manager.UpdateRun();
+                manager.playerScript?.SpawnPlayer();
+                manager.playerScript?.LoadRun();
+                manager.menus?.stateUnpause();
+            }
         }
     }
-    
 
+    private void LoadSceneWithProgress(int sceneIndex)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex));
+    }
 
-    
+    private IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        if (loadingScreen != null)
+            loadingScreen.SetActive(true);
+
+        if (progressSlider != null)
+            progressSlider.value = 0;
+
+        float startTime = Time.time;
+        float currentDisplayProgress = 0f;  // For smooth animation
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+        asyncLoad.allowSceneActivation = false;
+
+        while (asyncLoad.progress < 0.9f)
+        {
+            float targetProgress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+
+            currentDisplayProgress = Mathf.Lerp(currentDisplayProgress, targetProgress, Time.deltaTime * sliderSmoothingSpeed);
+
+            if (progressSlider != null)
+                progressSlider.value = currentDisplayProgress;
+
+            yield return null;
+        }
+
+        while (currentDisplayProgress < 0.99f)
+        {
+            currentDisplayProgress = Mathf.Lerp(currentDisplayProgress, 1f, Time.deltaTime * sliderSmoothingSpeed);
+
+            if (progressSlider != null)
+                progressSlider.value = currentDisplayProgress;
+
+            yield return null;
+        }
+        if (progressSlider != null)
+            progressSlider.value = 1f;
+
+        float elapsedTime = Time.time - startTime;
+        if (elapsedTime < minimumLoadTime)
+        {
+            float remainingTime = minimumLoadTime - elapsedTime;
+            yield return new WaitForSeconds(remainingTime);
+        }
+
+        asyncLoad.allowSceneActivation = true;
+
+        yield return new WaitForSeconds(0.1f);
+
+        if (loadingScreen != null)
+            loadingScreen.SetActive(false);
+    }
 }
